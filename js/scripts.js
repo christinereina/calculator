673c1c647e7c3c13551a9c4174a4d660e4276822
blacchoy@gmail.com <blacchoy@gmail.com>
2020-01-14 06:12:09 -0800

    added js

---
+++ js/scripts.js
@@ -0,0 +1,41 @@
+var saySomething = function(whatToSay) {
+  alert(whatToSay);
+};
+var add = function(number1, number2) {
+  return number1 + number2;
+};
+saySomething("hi");
+add(3, 5);
+var result = add(3, 5);
+alert(result);
+var add = function(number1, number2) {
+  return number1 + number2;
+};
+var add = function(number1, number2) {
+  return number1 + number2;
+};
+
+alert(add(10,5));
+var add = function(number1, number2) {
+  return number1 + number2;
+};
+
+var number1 = prompt("Enter a number:");
+
+alert(number1);
+var add = function(number1, number2) {
+  return number1 + number2;
+};
+
+var number1 = prompt("Enter a number:");
+var number2 = prompt("Enter another number:");
+
+alert(add(number1, number2));
+var add = function(number1, number2) {
+  return number1 + number2;
+};
+
+var number1 = parseInt(prompt("Enter a number:"));
+var number2 = parseInt(prompt("Enter another number:"));
+
+alert(add(number1, number2));
